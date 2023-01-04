7ccf618a01871781e9971bc1464362dcda3b9885
Sajal Tiwari <sajal.tiwari05@gmail.com>
2023-01-04 20:12:12 +0530

    updates

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+# import modules
+
+from .models import (
+    Product,Purchase,
+    Item,OrderItem,Order,
+)
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Purchase)
+
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order)
